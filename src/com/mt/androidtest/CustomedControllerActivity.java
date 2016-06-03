f7666cf09a0dfe3945d3602bd2a19a3c289c0ada
wwmengtao <wwmengtao@163.com>
2016-06-03 19:01:41 +0800

    说明
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- src/com/mt/androidtest/CustomedControllerActivity.java
+++ src/com/mt/androidtest/CustomedControllerActivity.java
@@ -2,7 +2,11 @@ package com.mt.androidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/**
+ * 自定义控件分为三类：自己绘制、组合、继承，即activity_customed_controller中的SelfDrawnView、CombinedView、InheritedView。
+ * @author Mengtao1
+ *
+ */
 public class CustomedControllerActivity extends Activity {
 
 	@Override
