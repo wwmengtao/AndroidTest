72493b91ae3fa7140d57b612b51a9722bc72bcf3
wwmengtao <wwmengtao@163.com>
2016-07-13 16:11:23 +0800

    调整
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- src/com/mt/androidtest/PackageManagerActivity.java
+++ src/com/mt/androidtest/PackageManagerActivity.java
@@ -1,38 +1,67 @@
 package com.mt.androidtest;
 
 import java.util.ArrayList;
-
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 
 public class PackageManagerActivity extends BaseActivity{
+	private ActivityManager mActivityManager=null;
+	private ArrayList<RunningServiceInfo> runningServices = null;
+	private String runningServiceName = null;
 	private String [] mMethodNameFT={
 			"showRunningServices",
+			"showCertainService",
+			"startCertainService"
 	};
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_base);
+		mActivityManager = (ActivityManager) PackageManagerActivity.this.getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
 		super.initListActivityData(null);
 		super.initListFTData(mMethodNameFT);
 	}
 	
-	private boolean showRunningServices() {  
-        ActivityManager mActivityManager = (ActivityManager) PackageManagerActivity.this.getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);  
-        ArrayList<RunningServiceInfo> runningServices = (ArrayList<RunningServiceInfo>) mActivityManager.getRunningServices(50);
-        String str_RunningService = null;
-        for (int i = 0; i < runningServices.size(); i++) {  
-        	str_RunningService = runningServices.get(i).service.getClassName().toString();
-            if (str_RunningService!=null) {  
-                ALog.Log("RS: "+str_RunningService); 
+	private void showRunningServices() {
+		runningServices = (ArrayList<RunningServiceInfo>) mActivityManager.getRunningServices(50);
+		if(null ==runningServices || 0==runningServices.size()){
+			return;
+		}
+        for (RunningServiceInfo  RSI : runningServices) {  
+        	runningServiceName = RSI.service.getClassName().toString();
+            if (runningServiceName!=null) {  
+                ALog.Log("RS: "+runningServiceName); 
             }  
         }
-        return false;  
-    }  
+    }
+	
+	private void showCertainService(String certainService){
+		runningServices = (ArrayList<RunningServiceInfo>) mActivityManager.getRunningServices(50);
+		if(null ==runningServices || 0==runningServices.size()){
+			return;
+		}
+        for (RunningServiceInfo  RSI : runningServices) {  
+        	runningServiceName = RSI.service.getClassName().toString();
+            if (runningServiceName!=null&&runningServiceName.equals(certainService)) {  
+                ALog.Log("CertainService: "+certainService);
+                return;
+            }  
+        }
+        ALog.Log("CertainService not found!");
+	}
+	
+	public void startCertainService(){
+		Intent mIntent = new Intent();
+		ComponentName mCn = new ComponentName("com.lenovo.widetouch","com.lenovo.widetouch.TouchService");
+		mIntent.setComponent(mCn);
+		startService(mIntent);
+	}
 	
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View view,	int position, long id) {
@@ -41,6 +70,13 @@ public class PackageManagerActivity extends BaseActivity{
 		switch(methodName){
 			case "showRunningServices":
 				showRunningServices();
+				break;
+			case "showCertainService":
+				String certainService = "com.lenovo.widetouch.TouchService";
+				showCertainService(certainService);
+				break;
+			case "startCertainService":
+				startCertainService();
 				break;
 		}
 	}
