d1090645bd8fc3f65f2f21881c1b830455be3f64
wwmengtao <wwmengtao@163.com>
2016-09-02 16:49:06 +0800

    修改
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- src/com/mt/androidtest/measurelayoutdraw/MeasureLayoutDrawActivity.java
+++ src/com/mt/androidtest/measurelayoutdraw/MeasureLayoutDrawActivity.java
@@ -6,17 +6,7 @@ import android.view.MenuItem;
 
 import com.mt.androidtest.BaseActivity;
 import com.mt.androidtest.R;
-/**
- * 一、根布局为LineaLayout时
- * 1)onMeasure执行了两次，原因是ViewRootImpl.performTraversals内部两次measureHierarchy都被调用了
- * 2)onLayout和onDraw各执行了一次
- * 二、根布局为RelativeLayout时
- * 1)onMeasure执行了四次，原因是ViewRootImpl.performTraversals内部measureHierarchy两次调用，并且每次
- * 执行measureHierarchy时，onMeasure都执行两次
- * 2)onLayout和onDraw各执行了一次
- * @author Mengtao1
- *
- */
+
 /**
  * 一、根布局为LineaLayout时
  * 1)onMeasure执行了两次，原因是ViewRootImpl.performTraversals内部两次measureHierarchy都被调用了
