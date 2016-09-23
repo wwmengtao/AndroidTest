d3df5a87ff4beddc6e764e2b256665b314e56d7c
wwmengtao <wwmengtao@163.com>
2016-09-23 10:33:26 +0800

    自定义view中取消Handler的使用，改用post
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- src/com/mt/androidtest/myselfview/SelfDrawnView.java
+++ src/com/mt/androidtest/myselfview/SelfDrawnView.java
@@ -1,19 +1,17 @@
 package com.mt.androidtest.myselfview;
 
-import com.mt.androidtest.ALog;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.os.Handler;
-import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class SelfDrawnView extends View implements OnClickListener,Handler.Callback {  
+import com.mt.androidtest.ALog;
+
+public class SelfDrawnView extends View implements OnClickListener {  
 	  
     private Paint mPaint;  
     private Rect mBounds;  
@@ -21,13 +19,11 @@ public class SelfDrawnView extends View implements OnClickListener,Handler.Callb
     private String text = "S/D click!";
     long mLastTime=0;
     long mCurTime=0;
-    private Handler handler = null;
     public SelfDrawnView(Context context, AttributeSet attrs) {  
         super(context, attrs);  
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);  
         mBounds = new Rect();  
         setOnClickListener(this);  
-        handler = new Handler(this);
     }  
   
     @Override  
@@ -42,13 +38,7 @@ public class SelfDrawnView extends View implements OnClickListener,Handler.Callb
         float textHeight = mBounds.height();  
         canvas.drawText(text, getWidth() / 2 - textWidth / 2, getHeight() / 2  
                 + textHeight / 2, mPaint);  
-    }  
-  
-	@Override
-	public void onDetachedFromWindow(){
-		super.onDetachedFromWindow();
-		handler.removeCallbacksAndMessages(null);
-	}
+    }
     
     @Override  
     public void onClick(View v) {  
@@ -57,28 +47,33 @@ public class SelfDrawnView extends View implements OnClickListener,Handler.Callb
         if(mCurTime-mLastTime<300){//双击事件
         	mCurTime =0;
             mLastTime = 0;
-            handler.removeMessages(1);
-            handler.sendEmptyMessage(2);
+            removeCallbacks(mSingleClickRunnable);
+            post(mDoubleClickRunnable);
         }else{//单击事件
-        	handler.sendEmptyMessageDelayed(1, 310);
+        	postDelayed(mSingleClickRunnable,310);
         }
+        invalidate();	
     }  
   
-	@Override
-	public boolean handleMessage(Message msg) {
-		// TODO Auto-generated method stub
-        switch (msg.what) {
-            case 1:
-            	ALog.Log("这是单击事件");
-                mCount++;  
-                text = String.valueOf(mCount);
-                break;
-            case 2:
-            	ALog.Log("这是双击事件");
-            	text = "doubleClick";
-                break;
-        }
-        invalidate();		
-		return false;
-	}
+    /**
+     * 自定义View中尽量不要使用Handler，使用post即可
+     */
+    private Runnable mSingleClickRunnable = new Runnable(){
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+        	ALog.Log("这是单击事件");
+            mCount++;  
+            text = String.valueOf(mCount);
+		}
+    };
+    
+    private Runnable mDoubleClickRunnable = new Runnable(){
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+        	ALog.Log("这是双击事件");
+        	text = "doubleClick";
+		}
+    };
 }  
